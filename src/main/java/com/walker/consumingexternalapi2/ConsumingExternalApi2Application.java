f0628abfac9a1b700e957903356b259e7740855f
Walker Negrao <1882452@tcs.com>
2023-12-07 16:38:33 -0300

    Criação da API

---
+++ src/main/java/com/walker/consumingexternalapi2/ConsumingExternalApi2Application.java
@@ -0,0 +1,13 @@
+package com.walker.consumingexternalapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingExternalApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingExternalApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/walker/consumingexternalapi2/ConsumingExternalApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.consumingexternalapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingExternalApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
